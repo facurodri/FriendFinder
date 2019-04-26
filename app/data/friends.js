ec40040e478f38c4d20c855e1af67c03fbb0f7bc
Facundo Rodriguez <facurodri91@gmail.com>
2019-04-26 12:43:01 -0400

    added few things

--- app/data/friends.js
+++ app/data/friends.js
@@ -24,15 +24,15 @@ var friendsArray = [
     "photo": "https://static1.squarespace.com/static/573be6d12fe1311b192d65f2/573bf639859fd06a1f80a4ec/58918536579fb38e73610a3e/1540418828850/Business+Photos+Head+Shots+Best+Profile+Photos.jpg?format=300w",
     "scores": [
       5,
+      4,
+      4,
+      2,
+      4,
+      3,
+      1,
       5,
       5,
-      5,
-      5,
-      5,
-      5,
-      5,
-      5,
-      5
+      3
     ]
 
   },
@@ -41,16 +41,16 @@ var friendsArray = [
     "photo": "https://static1.squarespace.com/static/573be6d12fe1311b192d65f2/573bf639859fd06a1f80a4ec/589158e7be65942a86db5749/1538446210803/profile+photos+personal+branding+headshots+auckland+photographer+focal+point+photos+-23.jpg?format=300w",
     "scores": [
       1,
+      2,
       1,
+      3,
+      2,
+      5,
+      2,
       1,
+      2,
       1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1,
-      1
+      2
     ]
 
   },
@@ -58,15 +58,15 @@ var friendsArray = [
     "profileName": "Parker",
     "photo": "https://static1.squarespace.com/static/573be6d12fe1311b192d65f2/574302ab2fe1312aea6871eb/5bb2afe57817f7d3ea24d9fa/1538437106997/profile+photos+headshots+team+photos+auckland+personal+branding+profile+photo+focal+point+photos+team+photos+staff+photos+best+photographer+awesome+photos+3.jpg?format=300w",
     "scores": [
+      4,
       2,
+      5,
       2,
+      3,
+      3,
+      1,
       2,
-      2,
-      2,
-      2,
-      2,
-      2,
-      2,
+      4,
       2
     ]
 
